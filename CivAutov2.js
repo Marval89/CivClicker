4ea779ff0f791645696fb69be7fcc235306f8827
Marval89 <kogut90@gmail.com>
2016-08-29 11:41:57 -0400

    Update CivAutov2.js

--- CivAutov2.js
+++ CivAutov2.js
@@ -1,3 +1,18 @@
-if (food.total<100) {
+function mainLoop {
+        if (food.total<100) {
         increment(food);
-    }
+        }
+}
+
+var runInterval = 100;      //How often to loop through logic
+var startupDelay = 2000;
+
+
+setTimeout(delayStart, startupDelay);
+function delayStart() {
+    setTimeout(delayStartAgain, startupDelay);
+}
+function delayStartAgain(){
+    setInterval(mainLoop, runInterval);
+    
+}
