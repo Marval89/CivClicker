251b165fd88d2c242335d0db869a1082f8c7b721
Marval89 <kogut90@gmail.com>
2016-08-29 13:23:14 -0400

    Update CivAutov2.js

--- CivAutov2.js
+++ CivAutov2.js
@@ -2,7 +2,16 @@
 
 var runInterval = 100;      //How often to loop through logic
 var startupDelay = 2000;
-
+function obliczenia(){
+    	//Calculate and update net production values for primary resources
+	if (population.current > 0 || population.zombies > 0){ //don't want to divide by zero
+		var netFood2 = (population.farmers * (1 + (efficiency.farmers * efficiency.happiness)) * (1 + efficiency.pestBonus)) * (1 + (wonder.food/10)) * (1 + walkTotal/120) * (1 + (mill.total/200) * (population.current/(population.current + population.zombies))) - population.current;
+	} else {
+		var netFood2 = (population.farmers * (1 + (efficiency.farmers * efficiency.happiness)) * (1 + efficiency.pestBonus)) * (1 + (wonder.food/10)) * (1 + walkTotal/120) * (1 + (mill.total/200)) - population.current;
+	}
+	var netWood2 = population.woodcutters * (efficiency.woodcutters * efficiency.happiness) * (1 + (wonder.wood/10));
+	var netStone2 = population.miners * (efficiency.miners * efficiency.happiness) * (1 + (wonder.stone/10));
+}
 
 setTimeout(delayStart, startupDelay);
 function delayStart() {
@@ -14,14 +23,15 @@ function delayStartAgain(){
 }
 
 function mainLoop() {
-        if (food.total<100) {
+    freeLand = Math.max(land - totalBuildings, 0)
+        if (food.total<100 || skins.total<2) {
         increment(food);}
         if(wood.total<20) {
             increment(wood);
         }
-        freeLand = Math.max(land - totalBuildings, 0)
+        
         if(wood.total>=20 && freeLand > 980){
-        createbuilding(whut,1);    
+        createBuilding(whut,1);    
         }
         
 }
