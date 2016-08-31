6ab95cf7e5a430ac327ba3105dddfa724b5a9714
Marval89 <kogut90@gmail.com>
2016-08-31 07:36:40 -0400

    Poprawa populacji

--- CivAutov2.js
+++ CivAutov2.js
@@ -28,10 +28,8 @@ function obliczenia(){
 	FoodPrzyrost = population.current / 20;
 	else if (population.current < 10000)
 	FoodPrzyrost = population.current / 10;
-	else if (population.current <100000)
+	else if (population.current <40000)
 	FoodPrzyrost = population.current / 5;
-	else if (population.current <1000000)
-	FoodPrzyrost = population.current / 3;
 	else
 	FoodPrzyrost = population.current /2;
 	 freeLand = Math.max(land - totalBuildings, 0);
@@ -80,7 +78,7 @@ function ZatrudniajFarmerow(){
 		 	createBuilding(apothecary,Przyrost);
 		hire('apothecaries',Przyrost);
 	}
-	else if(upgrades.masonry == 1 && population.clerics < 500000 && population.unemployed>0 && (population.clerics<population.miners/2 || population.clerics<population.labourers*10)){
+	else if(upgrades.masonry == 1 && population.clerics < 500000 && population.unemployed>0 && (population.clerics<population.miners*2 || population.clerics<population.labourers*10)){
 		if(freeLand > TargetFreeLand && population.clerics>temple.total-Przyrost && wood.total>=30 && stone.total>=120 && herbs.total >=10)
 			createBuilding(temple,Przyrost);
 		hire('clerics',Przyrost);
@@ -91,11 +89,13 @@ function ZatrudniajFarmerow(){
 	else if(population.unemployed>0 && (population.miners<=population.woodcutters || population.miners<population.labourers*50)){
 		hire('miners',Przyrost);
 	}
+	else
+		hire('farmers',Przyrost);
 		
 }
 
 function Magazyny(){
-	maxFoodA = Math.round(barn.total*200);
+	maxFoodA = Math.round(barn.total*400);
 	maxWoodA = Math.round(woodstock.total*200);
 	maxStoneA =Math.round(stonestock.total*200);
 	if(wood.total>100 && freeLand > TargetFreeLand){
